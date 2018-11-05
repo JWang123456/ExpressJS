da037f4457a4e43b35942eeb651c900bfba5b177
JWang123456 <wanggaojie913@yahoo.com>
2018-11-04 20:21:21 -0600

    done

--- airline/routes/index.js
+++ airline/routes/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 // var router = express.Router();
 
@@ -9,28 +10,95 @@ const express = require('express');
 // module.exports = router;
 const app = express();
 
+app.use(express.json());
+
+const flights = [
+  {id: 1, name: 'flight1'},
+  {id: 2, name: 'flight2'},
+  {id: 3, name: 'flight3'},
+  {id: 4, name: 'flight4'},
+
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+
 app.get('/api/flights', (req, res) => {
-  res.send(
-    {
-      "firstName": "John",
-      "lastName": "Smith",
-      "isAlive": true,
-      "age": 270
-      }
-    );
-})
+  res.send(flights);
+}); 
 
 app.get('/api/flights/:id', (req, res) => {
-  res.send(req.params);
-});
+  const flight = flights.find( flight => flight.id === parseInt(req.params.id));
+  if(!flight){
+    res.status(404).send('resourse not found :( ');  
+    return;
+  } 
+  res.send(flight);
+}); 
 
 app.get('/api/flights/:years/:month', (req, res) => {
   res.send(req.params);
 });
+
+app.post('/api/flights', (req, res) => {
+  
+    const {error} = validateFlight(req.body); 
+    
+    if(error){
+      res.status(400).send(error.details[0].message);
+      return; 
+    }
+
+    const flight = {
+      id: flights.length + 1,
+      name: req.body.name
+    };
+
+    flights.push(flight);
+    res.send(flight);
+});
+
+app.put('/api/flights/:id', (req, res) => {
+    const flight = flights.find( flight => flight.id === parseInt(req.params.id));
+    if(!flight){
+      res.status(404).send('resourse not found :( ');  
+      return;
+    } 
+    const { error } = validateFlight(req.body); 
+   
+    if(error){
+      res.status(400).send(error.details[0].message);
+      return; 
+    }
+
+    flight.name = req.body.name;
+    res.send(flight);
+});
+
+app.delete('/api/flights/:id', (req, res) => {
+  const flight = flights.find( flight => flight.id === parseInt(req.params.id));
+  if(!flight){
+    res.status(404).send('resourse not found :( ');  
+    return;
+  } 
+  
+  const index = flights.indexOf(flight);
+
+  flights.splice(index, 1);
+
+  res.send(flight);
+});
+
+
+function validateFlight(flight){
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(flight, schema);
+}
 
 const port = process.env.port || 3000;
 
